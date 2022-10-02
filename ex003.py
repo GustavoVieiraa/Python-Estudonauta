ab3929b7eacf040aff06a2a4ba4d567d76931a17
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-10-02 15:24:36 -0300

    Create ex003.py

---
+++ ex003.py
@@ -0,0 +1,6 @@
+#exercicio - 03, soma de valores.
+n1 = int(input("Informe um valor: "))
+n2 = int(input("Informe um segundo valor: "))
+s = n1 + n2
+#print('O resultado da soma é: ', s)
+print('O resultado da soma entre {} e {} é igual a {}'.format(n1, n2, s))
