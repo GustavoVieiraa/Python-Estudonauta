6adfd4487748a6f1c35b6e0a8318f2a31c3eb327
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-10-04 19:30:19 -0300

    Atualizações

--- ex012.py
+++ ex012.py
@@ -3,11 +3,11 @@
 
 print('=' * 15 + ' Promoção de 5% ' + '=' * 15)
 
-v_prod = float(input('Informe o valor do produto: '))
+v_prod = float(input('Informe o valor do produto: R$'))
 v_prod_desc = ((v_prod*5)/100)
 
-print('Valor do Produto: R${}'.format(v_prod))
-print('Valor do desconto: R${}'.format(v_prod_desc))
-print('Valor do Produto com o desconto de 5%: R${}'.format(v_prod-v_prod_desc))
+print('Valor do Produto: R${:.2f}'.format(v_prod))
+print('Valor do desconto: R${:.2f}'.format(v_prod_desc))
+print('Valor do Produto com o desconto de 5%: R${:.2f}'.format(v_prod-v_prod_desc))
 
 print('=' * 15 + ' Promoção de 5% ' + '=' * 15)
--- ex013.py
+++ ex013.py
@@ -3,12 +3,12 @@
 
 print('=' * 15 + ' Aumento de 15% Jr para Pl ' + '=' * 15)
 
-v_sal = float(input('Informe o seu salário: '))
+v_sal = float(input('Informe o seu salário: R$'))
 v_aum = ((v_sal*15)/100)
 
-print('Seu salário atual é R${}'.format(v_sal))
-print('Você teve um aumento de R${}, é referente a 15%'.format(v_aum))
-print('Salário novo: R${}'.format(v_sal+v_aum))
+print('Seu salário atual é R${:.2f}'.format(v_sal))
+print('Você teve um aumento de R${:.2f}, é referente a 15%'.format(v_aum))
+print('Salário novo: R${:.2f}'.format(v_sal+v_aum))
 
 
 print('=' * 15 + ' Aumento de 15% Jr para Pl ' + '=' * 15)
