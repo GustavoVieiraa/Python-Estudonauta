1d81322830e9c851a422ae36886c9be4616f1246
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-10-04 19:22:50 -0300

    Correção do ex 010

--- ex010.py
+++ ex010.py
@@ -4,6 +4,6 @@
 print('=' * 10 + 'Conversor de dolar' + '=' * 10)
 
 v_real = float(input('Valor em reais: '))
-v_dol = (v_real*3.27)
-print('Você pode comprar US${} dollinhos.'.format(v_dol))
+v_dol = (v_real/3.27)
+print('Você pode comprar US${:.2f} dollinhos.'.format(v_dol))
 print('=' * 10 + 'Conversor de dolar' + '=' * 10)
