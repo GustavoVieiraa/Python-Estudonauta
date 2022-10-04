106ca42f76c8e251239cf13d55194b539b129c78
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-10-03 23:27:03 -0300

    Correção
    arrumando a formula para raiz quadrada

--- ex006.py
+++ ex006.py
@@ -4,8 +4,8 @@ print('=--=' * 8)
 number = int(input('Digite um valor: '))
 num_dob = number * 2
 num_tri = number * 3
-num_raiz = number ** 2
+num_raiz = number ** (1/2)
 print('O dobro de {} é igual {}.'.format(number, num_dob))
 print('O triplo de {} é igual {}.'.format(number, num_tri))
-print('A raiz quadrada de {} é igual {}.'.format(number, num_raiz))
+print('A raiz quadrada de {} é igual {}'.format(number, num_raiz))
 print('=--=' * 8)
