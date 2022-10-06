b9349fb177abd3eed8364f2aa1ed8c857cdff4df
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-10-05 23:10:44 -0300

    correção do exercício
    finalizando o final do exercício e utilizando o rfind para verificar a última posição do 'A' conforme solicitado.

--- ex026.py
+++ ex026.py
@@ -5,10 +5,10 @@
 
 print('-' * 10 + ' Verificando Letras ' + '-' * 10)
 
-frase = str(input('Digite uma frase: '))
-print('A letra A aparece {} na frase ({}).'.format(frase.upper().count('A'), frase))
-print('A letra A aparece primeiro na posição {}'.format(frase.upper().find('A')))
-print('A letra A aparece pela ultima vez na posição {}'.format())
+frase = str(input('Digite uma frase: ')).strip()
+print('A letra A aparece {}x na frase ({}).'.format(frase.upper().count('A'), frase))
+print('A letra A aparece primeiro na posição {}'.format(frase.upper().find('A')+1))
+print('A letra A aparece pela ultima vez na posição {}'.format(frase.upper().rfind('A')+1))
 
 
 print('-' * 10 + ' Verificando Letras ' + '-' * 10)
