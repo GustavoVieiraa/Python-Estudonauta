e26a389a63507c78b8ac22fde438ab2b6fb8f4b6
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-10-09 23:35:00 -0300

    att

---
+++ ex032.py
@@ -0,0 +1,10 @@
+# Faça um programa que leia um ano qualquer e mostre se
+# ele é Bissexto.
+
+import time
+
+print('=-' * 10 + ' Ano bissexto? ' + '=-' * 10)
+
+ 
+
+print(ano_atual)
---
+++ ex033.py
@@ -0,0 +1,4 @@
+# Faça um programa que leia três números e mostre qual é o 
+# maior e o menor número.
+
+print()
