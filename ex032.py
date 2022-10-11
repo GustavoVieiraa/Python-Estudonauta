87c0ade0cd4e693c8417e18953416ff29fb4ac99
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-10-11 00:56:58 -0300

    correção e conclusão ex032
    exercício 032 concluído com êxito!

--- ex032.py
+++ ex032.py
@@ -1,10 +1,16 @@
 # Faça um programa que leia um ano qualquer e mostre se
 # ele é Bissexto.
 
-import time
+from datetime import date
 
 print('=-' * 10 + ' Ano bissexto? ' + '=-' * 10)
 
- 
+ano = int(input('Informe o ano para verificação | Digite 0 para analisar o ano atual: '))
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano {} é um ano BISSEXTO!'.format(ano))
+else:
+    print('O ano {} não é um ano BISSEXTO!'.format(ano))
 
-print(ano_atual)
+print('=-' * 10 + ' Ano bissexto? ' + '=-' * 10)
