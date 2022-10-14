3a1138a6b8153dbffc035678a91ce33eea860653
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-10-14 17:09:03 -0300

    exercício 046 concluído com sucesso!

---
+++ aula13teorica.py
@@ -0,0 +1,9 @@
+# Nessa aula aprendemos como utilizar estruturas de repetição FOR
+
+# Exemplo de estrutura de repetição FOR
+
+for c in range(1, 11):
+    print("{} ".format(c), end="")
+print()
+for c in range(10, 0, -1):
+    print("{} ".format(c), end="")
