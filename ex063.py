2e9cc4a6ddf1885edb9076eb705c6324b018ebba
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-11-14 21:48:03 -0300

    exercício 62 concluído e finalizado!

---
+++ ex063.py
@@ -0,0 +1,13 @@
+# Escreva um programa que leia um número n inteiro qualquer
+# e mostre na tela os n primeiros elemento de uma Sequência de Fibonacci.
+# ex. 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8
+
+
+print('=-' * 5 + ' Sequência de Fibonacci ' + '-=' * 5)
+
+c = 1
+
+while c < 11:
+    print(c)
+    c += 1
+print('=-' * 5 + ' Sequência de Fibonacci ' + '-=' * 5)
