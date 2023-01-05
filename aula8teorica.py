5579eb279ad22acf5fe9c4000238f6e2543c639d
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2023-01-04 23:36:25 -0300

    Delete aula8teorica.py

--- aula8teorica.py
+++
@@ -1,5 +0,0 @@
-# Nessa aula foi aprendido a importar as bibliotecas do Python e como realizar as 
-# instalações, também foi aprendido como importar diretamente da biblioteca
-# somente o que seria usado. ex: 
-
-from math import cos
