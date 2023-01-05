723757abda7c7d9d1454f0cc91212fe687f523cf
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2023-01-04 23:37:30 -0300

    Delete ex010.py

--- ex010.py
+++
@@ -1,9 +0,0 @@
-#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre
-#quantos dólares ela pode comprar, considere o dolar US$1,00 = R$3,27
-
-print('=' * 10 + 'Conversor de dolar' + '=' * 10)
-
-v_real = float(input('Valor em reais: '))
-v_dol = (v_real/3.27)
-print('Você pode comprar US${:.2f} dollinhos.'.format(v_dol))
-print('=' * 10 + 'Conversor de dolar' + '=' * 10)
