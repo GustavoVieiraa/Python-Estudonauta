b14ad8d4a635aecea0b56fca466564b34322aaf3
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2023-01-04 23:37:07 -0300

    Delete ex005.py

--- ex005.py
+++
@@ -1,7 +0,0 @@
-#Faça um programa que leia um número inteiro e mostre o seu sucessor e seu antecessor.
-
-print('=-=' * 10)
-num = int(input('Informe um número: '))
-print('O antecessor de {} é {}.'.format(num, (num-1)))
-print('O sucessor de {} é {}.'.format(num, (num+1)))
-print('=-=' * 10)
