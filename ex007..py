642f91f5a36824d4e97326a2abcd22684eeff836
Gustavo Vieira <98718305+GustavoVieiraa@users.noreply.github.com>
2023-01-04 23:37:15 -0300

    Delete ex007..py

--- ex007..py
+++
@@ -1,8 +0,0 @@
-# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
-
-print('=' * 30)
-n1 = int(input('Informe a primeira nota: '))
-n2 = int(input('Informe a segunda nota: '))
-med = (n1+n2)/2
-print('A média entre a primeira nota {} e a segunda nota {} é {}'.format(n1, n2, med))
-print('=' * 30)
